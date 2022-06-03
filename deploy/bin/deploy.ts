8887fc7dd458f32302fb9392972c7a6ba37cf624
Chris Seickel <cseickel@gmail.com>
2022-06-03 19:26:49 +0000

    build(quest): add cert, domain name, secret word

--- deploy/bin/deploy.ts
+++ deploy/bin/deploy.ts
@@ -5,6 +5,10 @@ import { RearcQuestStack } from '../lib/rearc-quest-stack';
 
 const app = new cdk.App();
 new RearcQuestStack(app, 'quest', {
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION
+  }
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
--- deploy/lib/rearc-quest-stack.ts
+++ deploy/lib/rearc-quest-stack.ts
@@ -1,10 +1,17 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { Vpc } from 'aws-cdk-lib/aws-ec2';
-import { Cluster, ContainerImage } from 'aws-cdk-lib/aws-ecs';
+import { Cluster, ContainerImage, Secret } from 'aws-cdk-lib/aws-ecs';
 import { DockerImageAsset } from "aws-cdk-lib/aws-ecr-assets";
 import { ApplicationLoadBalancedFargateService } from 'aws-cdk-lib/aws-ecs-patterns';
+import * as ssm from 'aws-cdk-lib/aws-ssm';
+import * as route53 from 'aws-cdk-lib/aws-route53';
+import * as elb from 'aws-cdk-lib/aws-elasticloadbalancingv2';
 import { join } from "path";
+import { LoadBalancerTarget } from 'aws-cdk-lib/aws-route53-targets';
+import { SslPolicy } from 'aws-cdk-lib/aws-elasticloadbalancingv2';
+import { Certificate, CertificateValidation } from 'aws-cdk-lib/aws-certificatemanager';
+import { listeners } from 'process';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class RearcQuestStack extends Stack {
@@ -13,6 +20,16 @@ export class RearcQuestStack extends Stack {
     const image = new DockerImageAsset(this, 'quest-image', {
       directory: join(__dirname, '../../')
     });
+
+    const domainZone = route53.HostedZone.fromLookup(this, 'quest-rearc-domain-zone', {
+      domainName: 'seickel.org',
+    });
+    const domainName = 'rearc-quest.seickel.org';
+    const certificate = new Certificate(this, 'alfresco-cert', {
+      domainName,
+      validation: CertificateValidation.fromDns(domainZone),
+    });
+
     const vpc = new Vpc(this, 'quest-rearc-vpc', {});
     const cluster = new Cluster(this, 'quest-rearc-cluster', { vpc });
     const srv = new ApplicationLoadBalancedFargateService(this, 'quest-rearc-service', {
@@ -22,9 +39,21 @@ export class RearcQuestStack extends Stack {
       desiredCount: 1,
       taskImageOptions: {
         image: ContainerImage.fromDockerImageAsset(image),
-        containerPort: 3000
+        containerPort: 3000,
+        secrets: {
+          'SECRET_WORD': Secret.fromSsmParameter(
+            ssm.StringParameter.fromSecureStringParameterAttributes(this, 'idb-basic-auth-user', {
+              parameterName: '/rearc/quest/SECRET_WORD',
+              simpleName: false,
+              version: 1,
+            })
+          )
+        }
       },
-      publicLoadBalancer: true
+      publicLoadBalancer: true,
+      certificate,
+      domainName,
+      domainZone
     });
   }
 }
